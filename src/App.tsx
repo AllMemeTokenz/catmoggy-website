7171b709ca8e322601e309bd0032e46b09588db2
RafifMMR <rafifmuhammad2003@gmail.com>
2025-03-19 13:53:50 +0700

    fix next

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import ErrorPage from "./apps/error_page"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ErrorPage from "./apps/error_page";
 import HomePage from "./apps/home_page";
 import ExportNav from "./components/ui/nav/export_nav";
 import Footer from "./components/ui/nav/footer";
 
 const App = () => {
   return (
-      <BrowserRouter basename={import.meta.env.PROD ? '/catmoggy-website' : '/'}>
-         <ExportNav />
-         <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="*" element={<ErrorPage />} />
-         </Routes> 
-         <Footer />
-      </BrowserRouter>
-  )
-}
+    <BrowserRouter basename="/">
+      <ExportNav />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
 export default App;
-
-
